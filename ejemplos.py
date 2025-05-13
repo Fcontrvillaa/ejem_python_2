da4cf1711a03d6fe8010fca3bab854cd431a8860
Fcontrvillaa <fcontreras+1@gmail.com>
2025-05-13 01:13:12 +0000

    ejem

---
+++ ejemplos.py
@@ -0,0 +1,27 @@
+
+
+
+
+# lista 0 (2,3,4,65,4)
+
+
+# for index in lista:
+#     print(f)
+
+
+# diccionario = {
+#     'color1': 'anmarillo',
+#     'color2': 'rojo',
+#     'color3': 'verde'
+# }
+
+# for clave, valor in diccionario.items():
+#     print (f"clave: {clave} , valor : {valor}")
+
+
+contador = 10
+
+while contador > 0:
+    contador = contador - 1
+    
+    print("imprime")
